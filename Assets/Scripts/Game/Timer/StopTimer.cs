43564f585826502a6869747c6a6d0bac3233c3c8
Manihams <64426110+Manihams@users.noreply.github.com>
2025-05-08 20:41:17 -0400

    Update StopTimer.cs

--- Assets/Scripts/Game/Timer/StopTimer.cs
+++ Assets/Scripts/Game/Timer/StopTimer.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class StopTimer : MonoBehaviour
 {
+    // This method will stop the timer when it's called
     public void Stoptimer()
     {
+        // This function will find the instance of the timer class
         Timer timer = FindObjectOfType<Timer>();
         if (timer != null)
         {
