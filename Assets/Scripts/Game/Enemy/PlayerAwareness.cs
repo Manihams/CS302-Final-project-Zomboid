82214d51489c06cfb6679e7db3116c69f15b2e21
Manihams <64426110+Manihams@users.noreply.github.com>
2025-05-08 20:32:39 -0400

    Update PlayerAwareness.cs

--- Assets/Scripts/Game/Enemy/PlayerAwareness.cs
+++ Assets/Scripts/Game/Enemy/PlayerAwareness.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerAwareness : MonoBehaviour
 {
+    // To let enemy know if it is aware of player 
     public bool AwareOfPlayer { get; private set; }
 
+    // Stores the direction from the enemy to the player as unit vector
     public Vector2 DirectionToPlayer { get; private set; }
 
+    // Max distance for enemies to detect the player
     [SerializeField]
     private float _playerAwarenessDistance;
 
     private Transform _player;
 
+    // Finds the player's transform by locating the player movement by calling the script 
     private void Awake(){
         _player = FindObjectOfType<PlayerMovement>().transform;
     }
@@ -20,9 +24,11 @@ public class PlayerAwareness : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Calculates the vector from the enemy to the player
         Vector2 enemyToPlayerVector = _player.position - transform.position;
         DirectionToPlayer = enemyToPlayerVector.normalized;
 
+        // Checks if the player is close to the enemy
         if(enemyToPlayerVector.magnitude <= _playerAwarenessDistance)
         {
             AwareOfPlayer = true;
