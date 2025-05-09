72fd4f5e7cb593952661bf598be6d5e6f7050f1e
adithya00001 <pradisrini@gmail.com>
2025-05-08 20:19:51 -0400

    Update PlayerDamagedInvincibility.cs

--- Assets/Scripts/Game/Player/PlayerDamagedInvincibility.cs
+++ Assets/Scripts/Game/Player/PlayerDamagedInvincibility.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    // Duration for which the player is invincible
     [SerializeField]
     private float _invincibilityDuration;
 
     private InvincibilityController _invincibilityController;
 
+    // Get reference to the InvincibilityController component
     private void Awake()
     {
         _invincibilityController = GetComponent<InvincibilityController>();
     }
 
+    // Call this method to start invincibility for the set duration
     public void StartInvincibility()
     {
         _invincibilityController.StartInvincibility(_invincibilityDuration);
     }
 }
-
