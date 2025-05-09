d4b34faf549d777400bad6e66acdcbaf6d7fa48b
Manihams <64426110+Manihams@users.noreply.github.com>
2025-05-08 20:45:54 -0400

    Update GameOverHandler.cs

--- Assets/Scripts/Game/gameover/GameOverHandler.cs
+++ Assets/Scripts/Game/gameover/GameOverHandler.cs
@@ -3,21 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class GameOverHandler : MonoBehaviour
 {
+    // This variable will display the game over screen after player is killed
     [SerializeField] private GameObject gameOverPanel;
 
-    
+
+    // Displays the game over screen and also pause the game in the background
     public void ShowGameOver()
     {
         gameOverPanel.SetActive(true);
         Time.timeScale = 0f;
     }
 
+    // This will restart the game after player hits restart button
     public void RestartGame()
     {
         Time.timeScale = 1f; // Resume time
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+    // Quits the game and loads the main menu screen
     public void QuitGame()
     {
         SceneManager.LoadScene(0);
