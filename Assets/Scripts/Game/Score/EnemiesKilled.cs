8a2d3aa09c456052b2e6c9976a56e140e0164b11
Manihams <64426110+Manihams@users.noreply.github.com>
2025-05-08 20:36:41 -0400

    Update EnemiesKilled.cs

--- Assets/Scripts/Game/Score/EnemiesKilled.cs
+++ Assets/Scripts/Game/Score/EnemiesKilled.cs
@@ -3,12 +3,15 @@ using TMPro; // Import the TextMeshPro namespace
 
 public class EnemiesKilled : MonoBehaviour
 {
+    // Allows global access to track the score
     public static EnemiesKilled Instance;
 
     [SerializeField] private TextMeshProUGUI scoreText; // Assign in the Inspector
 
+    // Stores current score
     private int _score = 0;
 
+    // Reference to playerPowerUp which boosts player's health after killing zombies
     private PlayerPowerUp _playerPowerUp;
 
     private void Awake()
@@ -23,15 +26,17 @@ public class EnemiesKilled : MonoBehaviour
             Destroy(gameObject);
         }
 
+        // Calls the PlayerPowerUp for storing kills
         _playerPowerUp = FindObjectOfType<PlayerPowerUp>();
     }
 
-    // Call this method to increase the score
+    // This method us used to increase the score
     public void AddScore(int amount)
     {
         _score += amount;
         UpdateScoreUI();
 
+        // Increases player's health every 5 kills
         if (_score % 5 == 0 && _playerPowerUp != null)
         {
             _playerPowerUp.RegisterKill();
