3da660cf1f49a19912fb975a76ddae6ace01af2e
Manihams <64426110+Manihams@users.noreply.github.com>
2025-05-08 20:17:15 -0400

    Update EnemyMovement.cs

--- Assets/Scripts/Game/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Game/Enemy/EnemyMovement.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    // Stores enemy speed
     [SerializeField]
     private float _speed;
 
+    // Stores the rotation speed towards enemy
     [SerializeField]
     private float _rotationSpeed;
 
+    // checks if the zombie is at the edge of the screen
     [SerializeField]
     private float _screenBorder;
 
@@ -21,6 +24,7 @@ public class EnemyMovement : MonoBehaviour
 
     private void Awake()
     {
+        // Initialized all the components and variables
         _rigidbody = GetComponent<Rigidbody2D>();
         _playerAwareness = GetComponent<PlayerAwareness>();
         _targetDirection = transform.up;
@@ -28,6 +32,9 @@ public class EnemyMovement : MonoBehaviour
     }
 
     // Update is called once per frame
+    // tracks enemy's movement
+    // Rotate the face towards the player
+    // moves at constant velocity
     private void FixedUpdate()
     {
         UpdateTargetDirection();
@@ -35,6 +42,7 @@ public class EnemyMovement : MonoBehaviour
         SetVelocity();
     }
 
+    // Adjusts the direction, tracks player, and adjust movement near screen edges
     private void UpdateTargetDirection()
     {
         HandleRandomDirectionChange();
@@ -42,6 +50,7 @@ public class EnemyMovement : MonoBehaviour
         HandleEnemyOffScreen();
     }
 
+    // Controls the zombie directions and postion them at random places
     private void HandleRandomDirectionChange()
     {
         _changeDirectionCooldown -= Time.deltaTime;
@@ -55,6 +64,7 @@ public class EnemyMovement : MonoBehaviour
         }
     }
 
+    // If the enemy detects the player, changes the direction towards the player
     private void HandlePlayerTargeting()
     {
         if(_playerAwareness.AwareOfPlayer)
@@ -65,19 +75,23 @@ public class EnemyMovement : MonoBehaviour
 
     private void HandleEnemyOffScreen()
     {
+        // Converts the world position to screen coordinates
          Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
 
+        // Changes its horizontal direction if it's near the screen edge
         if((screenPosition.x < _screenBorder && _targetDirection.x < 0) || (screenPosition.x > _camera.pixelWidth - _screenBorder && _targetDirection.x > 0))
         {
             _targetDirection = new Vector2(-_targetDirection.x, _targetDirection.y);
         }
-
+        
+        // Changes its vertical direction if it's near the screen edge
         if((screenPosition.y < _screenBorder && _targetDirection.y < 0) || (screenPosition.y > _camera.pixelHeight - _screenBorder && _targetDirection.y > 0))
         {
             _targetDirection = new Vector2(_targetDirection.x, -_targetDirection.y);
         }
     }
 
+    // Calculates the target rotation and moves toward the target at a set speed
     private void RotateTowardsTarget()
     {
         Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetDirection);
@@ -86,6 +100,7 @@ public class EnemyMovement : MonoBehaviour
         _rigidbody.SetRotation(rotation);
     }
 
+    // Moves the enemy forward at a set speed
     private void SetVelocity()
     {
         _rigidbody.linearVelocity = transform.up * _speed;
