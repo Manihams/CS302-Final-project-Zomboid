46f31f4409954a4bf17303fb50bb0b25a87a5b80
Manihams <64426110+Manihams@users.noreply.github.com>
2025-05-08 20:07:04 -0400

    Update EnemyAttack.cs

--- Assets/Scripts/Game/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Game/Enemy/EnemyAttack.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
+    // amount of damage the enemy does when collides with the player
     [SerializeField]
     private float _damageAmount;
+        // This function is called when the enemy contact another 2D collider
        private void OnCollisionStay2D(Collision2D collision)
        {
+            // Checks if it collides with the player
             if(collision.gameObject.GetComponent<PlayerMovement>())
             {
+                // Health bar is updated when the enemy attacks the player
                 var healthController = collision.gameObject.GetComponent<HealthController>();
-            
+
+                // Player's health changes when the zombie attacks
                 healthController.TakeDamage(_damageAmount);
             }
         }
