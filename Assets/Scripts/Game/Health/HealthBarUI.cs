d7a50d872ee0d071267496322f573e27a35566c4
adithya00001 <pradisrini@gmail.com>
2025-05-08 20:32:30 -0400

    Update HealthBarUI.cs

--- Assets/Scripts/Game/Health/HealthBarUI.cs
+++ Assets/Scripts/Game/Health/HealthBarUI.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class HealthBarUI : MonoBehaviour
 {
     [SerializeField]
-    private UnityEngine.UI.Image _healthBarForeGroundImage;
+    private UnityEngine.UI.Image _healthBarForeGroundImage;  // Reference to the health bar foreground image
 
+    // Update the health bar UI based on the player's current health percentage
     public void UpdateHealthBar(HealthController healthController)
     {
+        // Set the fill amount of the health bar based on the health percentage
         _healthBarForeGroundImage.fillAmount = healthController.HealthPercentage;
     }
 }
