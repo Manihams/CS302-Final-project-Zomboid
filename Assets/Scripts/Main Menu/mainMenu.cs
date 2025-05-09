e34e30f4781203cc5b270cc30814cb61111f1dc8
adithya00001 <pradisrini@gmail.com>
2025-05-08 20:49:04 -0400

    Update mainMenu.cs

--- Assets/Scripts/Main Menu/mainMenu.cs
+++ Assets/Scripts/Main Menu/mainMenu.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
+    // Just loading the scene with build index 1
     public void play()
     {
         SceneManager.LoadScene(1);
